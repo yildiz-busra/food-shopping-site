22d56f005be10a0b955e72c9a95d775b39f80b4b
Büşra Yıldız <402515@ogr.ktu.edu.tr>
2024-08-09 20:45:41 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import Navi from "./Navi";
+import CategoryList from "./CategoryList";
+import ProductList from "./ProductList";
+
+import { Col, Container, Row } from "reactstrap";
+
+class App extends Component {
+  state = { currentCategory: "", products: [] };
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  changeCategory = (category) => {
+    this.setState({ currentCategory: category.categoryName });
+    this.getProducts(category.id)
+  };
+
+  getProducts = categoryId => {
+    let url = "http://localhost:3000/products";
+    if (categoryId) {
+      url += "?categoryId=" + categoryId;
+    }
+    fetch(url)
+      .then((response) => response.json())
+      .then((response) => this.setState({ products: response }));
+  };
+
+  render() {
+    let categoryInfo = { title: "Catagories" };
+    let productInfo = { title: "Products" };
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Navi />
+          </Row>
+          <Row>
+            <Col xs="3">
+              <CategoryList
+                currentCategory={this.state.currentCategory}
+                changeCategory={this.changeCategory}
+                info={categoryInfo}
+              />
+            </Col>
+            <Col xs="9 ">
+              <ProductList
+                products={this.state.products}
+                currentCategory={this.state.currentCategory}
+                info={productInfo}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Navi.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Navi extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Navigation Bar</h2>
+                <br></br>
+            </div>
+        );
+    }
+}
+
+export default Navi;
